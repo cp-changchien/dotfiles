6759a53db6ec466cc87b523570eb8d7f0f28dd8c
Cooper <67753381+cp-changchien@users.noreply.github.com>
2024-07-31 22:18:18 +0800

    add sketchybar

---
+++ .config/sketchybar/items/music.sh
@@ -0,0 +1,26 @@
+#!/bin/env/bash
+
+# Load global styles, colors and icons
+source "$CONFIG_DIR/globalstyles.sh"
+
+music=(
+  "${bracket_defaults[@]}"
+  script="$PLUGIN_DIR/music.sh"
+  popup.align=center
+  padding_left=150
+  label.padding_right=$PADDINGS
+  padding_right=$(($PADDINGS * 1))
+  drawing=off
+  label="Loading…"
+  background.image=media.artwork
+  background.image.scale=0.75
+  background.image.corner_radius=$PADDINGS
+  icon.padding_left=37
+  label.max_chars=17
+  updates=on
+  --subscribe music media_change
+)
+
+sketchybar                       \
+  --add item music right         \
+  --set      music "${music[@]}"
